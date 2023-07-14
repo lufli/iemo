3f5c81c7a0974ea19e4f142b6a5fef971a70b2eb
ruoyu <ruoyu1993@gmail.com>
2023-07-14 12:50:08 -0400

    common navigation layout

---
+++ components/navigation.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Link from 'next/link'
+import { List } from 'antd'
+
+const myApps = [
+  {
+    'title': 'One Submit! No Regret!',
+    'description': 'Having trouble making decisions? Don\'t worry! We will help you!',
+    'link': '/decision-maker'
+  },
+  {
+    'title': 'Become A Billionaire',
+    'description': 'Are you a billionaire?',
+    'link': '/billionaire'
+  }
+]
+
+export default function Navigation () {
+  return (
+    <List
+      header={<h2>A Collection of Coolest Tools</h2>}
+      dataSource={myApps}
+      renderItem={(item) => (
+        <List.Item>
+          <List.Item.Meta
+            title={<Link href={item.link}>{item.title}</Link>}
+            description={item.description}
+          />
+        </List.Item>
+      )}
+    />
+  )
+}
