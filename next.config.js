fe5a5a5345fa01e962e2a97c8308ffba92bed8ca
Lufan Li <lufanli@Lufans-MBP.fios-router.home>
2023-07-15 04:41:32 -0400

    disable lint when build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    // TODO: enable lint
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
