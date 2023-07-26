b340e138e6626e262af8588582c1a45139af51a3
Lufan Li <lufanli@Lufans-MBP.fios-router.home>
2023-07-25 20:31:10 -0400

    try github webhooks

--- __tests__/lib/slack/message.js
+++ __tests__/lib/slack/message.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { sendMessageToChannel } from '@/lib/slack/message';
+import { sendMessageToChannel, sendMessageToUser } from '@/lib/slack/message';
 
 jest.mock('axios');
 
@@ -7,7 +7,6 @@ describe('lib/slack/message', () => {
   const channel = '#test_channel';
   const webhookUrl = process.env.SLACK_WEBHOOK_URL;
   const requestOptions = {
-    method: 'POST',
     headers: { 'Content-Type': 'application/json' },
   };
 
@@ -133,6 +132,126 @@ describe('lib/slack/message', () => {
     axios.post.mockRejectedValueOnce(new Error('Error sending data'));
 
     await sendMessageToChannel({});
+    expect(consoleErrorSpy).toHaveBeenCalled();
+  });
+
+  it('sendMessageToUser sending warning messages', async () => {
+    const level = 'good';
+    const text = 'good text';
+    const link = 'https://www.iemo.io';
+    const payload = {
+      channel,
+      attachments: [
+        {
+          mrkdwn_in: ['text'],
+          color: level,
+          text,
+          author_name: ':white_check_mark: status: good',
+          title: 'Read more...',
+          title_link: link,
+          footer: 'iEmo',
+          ts: Date.now(),
+        },
+      ],
+    };
+    const responseData = { message: 'Data successfully sent' };
+    axios.post.mockResolvedValueOnce({ data: responseData });
+    await sendMessageToUser({
+      channel, level, text, link,
+    });
+
+    expect(axios.post).toHaveBeenCalledWith(webhookUrl, payload, requestOptions);
+  });
+
+  it('sendMessageToUser sending warning message', async () => {
+    const level = 'warning';
+    const text = 'warning text';
+    const link = 'https://www.iemo.io';
+    const payload = {
+      channel,
+      attachments: [
+        {
+          mrkdwn_in: ['text'],
+          color: level,
+          text,
+          author_name: ':warning: status: warning',
+          title: 'Read more...',
+          title_link: link,
+          footer: 'iEmo',
+          ts: Date.now(),
+        },
+      ],
+    };
+    const responseData = { message: 'Data successfully sent' };
+    axios.post.mockResolvedValueOnce({ data: responseData });
+    await sendMessageToUser({
+      channel, level, text, link,
+    });
+
+    expect(axios.post).toHaveBeenCalledWith(webhookUrl, payload, requestOptions);
+  });
+
+  it('sendMessageToUser sending danger message', async () => {
+    const level = 'danger';
+    const text = 'danger text';
+    const link = 'https://www.iemo.io';
+    const payload = {
+      channel,
+      attachments: [
+        {
+          mrkdwn_in: ['text'],
+          color: level,
+          text,
+          author_name: ':x: status: error',
+          title: 'Read more...',
+          title_link: link,
+          footer: 'iEmo',
+          ts: Date.now(),
+        },
+      ],
+    };
+    const responseData = { message: 'Data successfully sent' };
+    axios.post.mockResolvedValueOnce({ data: responseData });
+    await sendMessageToUser({
+      channel, level, text, link,
+    });
+
+    expect(axios.post).toHaveBeenCalledWith(webhookUrl, payload, requestOptions);
+  });
+
+  it('sendMessageToUser sending default message', async () => {
+    const level = undefined;
+    const text = 'default text';
+    const link = 'https://www.iemo.io';
+    const payload = {
+      channel,
+      attachments: [
+        {
+          mrkdwn_in: ['text'],
+          color: level,
+          text,
+          author_name: ':ghost: Notification',
+          title: 'Read more...',
+          title_link: link,
+          footer: 'iEmo',
+          ts: Date.now(),
+        },
+      ],
+    };
+    const responseData = { message: 'Data successfully sent' };
+    axios.post.mockResolvedValueOnce({ data: responseData });
+    await sendMessageToUser({
+      channel, level, text, link,
+    });
+
+    expect(axios.post).toHaveBeenCalledWith(webhookUrl, payload, requestOptions);
+  });
+
+  it('sendMessagToUser handle error when reject', async () => {
+    const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation();
+    axios.post.mockRejectedValueOnce(new Error('Error sending data'));
+
+    await sendMessageToUser({});
     expect(consoleErrorSpy).toHaveBeenCalled();
   });
 });
