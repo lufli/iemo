8754e4f9c7dfe4fbff4d10d717cf7a1d069867c9
Lufan Li <lufanli@Lufans-MBP.fios-router.home>
2023-08-01 18:41:01 -0400

    improve daily api

--- pages/api/daily.js
+++ pages/api/daily.js
@@ -2,8 +2,8 @@ import { main } from '@/scripts/daily';
 
 export default async function handler(req, res) {
   try {
-    await main();
-    res.status(200).json({ message: 'Ok' });
+    const result = await main();
+    res.status(200).json(result);
   } catch (error) {
     res.status(400).json({ message: 'Error' });
   }
