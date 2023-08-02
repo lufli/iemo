08a4cd9f015deb1b1888246a867b3e67e21fd12b
lufli <lufan.li@outlook.com>
2023-08-02 08:42:36 -0400

    improve daily api (#19)
    Co-authored-by: Lufan Li <lufanli@Lufans-MBP.fios-router.home>

--- scripts/daily.js
+++ scripts/daily.js
@@ -1,8 +1,10 @@
 const { kv } = require('@vercel/kv');
 const { sendMessageToChannel } = require('../lib/slack/message');
-const getPowerballWinningNumbers = require('../lib/lottery/powerball').getWinningNumbers;
-const getDoublePlayWinningNumbers = require('../lib/lottery/double-play').getWinningNumbers;
-const getMegaWinningNumbers = require('../lib/lottery/mega').getWinningNumbers;
+const powerball = require('../lib/lottery/powerball');
+const doublePlay = require('../lib/lottery/double-play');
+const mega = require('../lib/lottery/mega');
+
+const ERROR_ON_MESSAGE = { message: 'Error on sending message' };
 
 async function main() {
   // Script runs every hour.
@@ -11,9 +13,9 @@ async function main() {
   // Tue -> Powerball, Double Play
   // Wed -> Mega
   // Thu -> Powerball, Double Play
-  // Fri ->
+  // Fri -> Report
   // Sat -> Mega
-  // Sun -> Powerball, Double Play
+  // Sun -> Reset Prize, Powerball, Double Play
   const day = new Date().toLocaleDateString('en-US', {
     weekday: 'short',
     timeZone: 'America/New_York',
@@ -22,56 +24,141 @@ async function main() {
   let powerballWinningNumbers = null;
   let doublePlayWinningNumbers = null;
   let megaWinningNumbers = null;
+  const updates = [];
+  const errors = [];
 
   try {
     if (day === 'Tue') {
-      powerballWinningNumbers = await getPowerballWinningNumbers();
-      doublePlayWinningNumbers = await getDoublePlayWinningNumbers();
+      powerballWinningNumbers = await powerball.getWinningNumbers();
+      doublePlayWinningNumbers = await doublePlay.getWinningNumbers();
     } else if (day === 'Wed') {
-      megaWinningNumbers = await getMegaWinningNumbers();
+      megaWinningNumbers = await mega.getWinningNumbers();
     } else if (day === 'Thu') {
-      powerballWinningNumbers = await getPowerballWinningNumbers();
-      doublePlayWinningNumbers = await getDoublePlayWinningNumbers();
-    } else if (day === 'Sat') {
-      megaWinningNumbers = await getMegaWinningNumbers();
-    } else if (day === 'Sun') {
-      powerballWinningNumbers = await getPowerballWinningNumbers();
-      doublePlayWinningNumbers = await getDoublePlayWinningNumbers();
-    }
+      powerballWinningNumbers = await powerball.getWinningNumbers();
+      doublePlayWinningNumbers = await doublePlay.getWinningNumbers();
+    } else if (day === 'Fri') {
+      // Fri send a weekly report
+      const powerballPrize = await kv.get('powerball_prize');
+      const doublePlayPrize = await kv.get('double_play_prize');
+      const megaPrize = await kv.get('mega_prize');
 
-    const updates = [];
+      const text = `
+Weekly Report
+Powerball Prize: ${powerballPrize}
+Double Play Prize: ${doublePlayPrize}
+Mega Millions Prize: ${megaPrize}
+
+Total: ${powerballPrize + doublePlayPrize + megaPrize}`;
+
+      const response = await sendMessageToChannel({
+        channel: '#billionaire-monitor',
+        text,
+        link: 'https://www.iemo.io/weekly_report',
+      });
+
+      if (response !== 'ok') {
+        return ERROR_ON_MESSAGE;
+      }
+
+      updates.push('Weekly Report');
+    } else if (day === 'Sat') {
+      megaWinningNumbers = await mega.getWinningNumbers();
+    } else if (day === 'Sun') {
+      await kv.set('powerball_prize', 0);
+      await kv.set('double_play_prize', 0);
+      await kv.set('mega_prize', 0);
+
+      powerballWinningNumbers = await powerball.getWinningNumbers();
+      doublePlayWinningNumbers = await doublePlay.getWinningNumbers();
+    }
 
     if (powerballWinningNumbers) {
-      await kv.set('powerball', powerballWinningNumbers);
-      updates.push('Powerball');
+      // update powerball winning numbers
+      const response = await kv.set('powerball', powerballWinningNumbers);
+      if (response !== 'OK') {
+        errors.push('Error on updating Powerball winning numbers');
+      } else {
+        updates.push('Powerball');
+      }
+      // incr powerball prize this week
+      const powerballPlays = await kv.get('powerball_plays');
+      const prize = powerballPlays.reduce((sum, play) => (
+        sum + powerball.calculatePrize(powerballWinningNumbers, play)
+      ), 0);
+      await kv.incrby('powerball_prize', prize);
     }
+
     if (doublePlayWinningNumbers) {
-      await kv.set('double-play', doublePlayWinningNumbers);
-      updates.push('Double Play');
+      // update double play winning numbers
+      const response = await kv.set('double_play', doublePlayWinningNumbers);
+      if (response !== 'OK') {
+        errors.push('Error on updating Double Play winning numbers');
+      } else {
+        updates.push('Double Play');
+      }
+      // incr double play prize this week
+      const doublePlayPlays = await kv.get('powerball_plays');
+      const prize = doublePlayPlays.reduce((sum, play) => (
+        sum + powerball.calculatePrize(doublePlayWinningNumbers, play)
+      ), 0);
+      await kv.incrby('double_play_prize', prize);
     }
+
     if (megaWinningNumbers) {
-      await kv.set('mega', megaWinningNumbers);
-      updates.push('Mega Millions');
+      // update mega millions winning number
+      const response = await kv.set('mega', megaWinningNumbers);
+      if (response !== 'OK') {
+        errors.push('Error on updating Mega Millions winning numbers');
+      } else {
+        updates.push('Mega Millions');
+      }
+      // incr mega millions prize this week
+      const megaPlays = await kv.get('mega_plays');
+      const prize = megaPlays.reduce((sum, play) => (
+        sum + powerball.calculatePrize(megaWinningNumbers, play)
+      ), 0);
+      await kv.incrby('mega_prize', prize);
     }
-    if (updates.length === 0) {
-      sendMessageToChannel({
+
+    if (errors.length !== 0) {
+      const response = await sendMessageToChannel({
+        channel: '#billionaire-monitor',
+        level: 'danger',
+        text: errors.join('\n'),
+      });
+      if (response !== 'ok') {
+        return ERROR_ON_MESSAGE;
+      }
+    } else if (updates.length === 0) {
+      const response = await sendMessageToChannel({
         channel: '#billionaire-monitor',
         text: 'Cron job run, but did nothing.',
       });
+
+      if (response !== 'ok') {
+        return ERROR_ON_MESSAGE;
+      }
     } else {
-      sendMessageToChannel({
+      const response = await sendMessageToChannel({
         channel: '#billionaire-monitor',
         level: 'good',
         text: `${updates.join(', ')} updated!`,
       });
+      if (response !== 'ok') {
+        return ERROR_ON_MESSAGE;
+      }
     }
   } catch (error) {
-    sendMessageToChannel({
+    const response = sendMessageToChannel({
       channel: '#billionaire-monitor',
       level: 'danger',
       text: error.message,
     });
+    if (response !== 'ok') {
+      return ERROR_ON_MESSAGE;
+    }
   }
+  return { message: 'Ok' };
   // Nothing to check now.
 }
 
