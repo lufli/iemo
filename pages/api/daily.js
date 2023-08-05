6fa7e36666c78eb40f9e4f6811993ae93b3d496a
lufli <lufan.li@outlook.com>
2023-08-05 02:58:27 -0400

    daily api resurn (#20)
    Co-authored-by: Lufan Li <lufanli@Lufans-MBP.fios-router.home>

--- pages/api/daily.js
+++ pages/api/daily.js
@@ -3,7 +3,7 @@ import { main } from '@/scripts/daily';
 export default async function handler(req, res) {
   try {
     const result = await main();
-    res.status(200).json(result);
+    res.status(200).json({ data: result });
   } catch (error) {
     res.status(400).json({ message: 'Error' });
   }
